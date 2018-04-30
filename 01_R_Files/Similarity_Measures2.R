b540ed984c7befe636a023b7bdaca78a803de21e
Nils Ratnaweera <rata@zhaw.ch>
2018-04-30 13:47:52 +0200

    simillarity measures

---
+++ 01_R_Files/Similarity_Measures2.R
@@ -0,0 +1,28 @@
+
+
+peds <- read_delim("../CMA_FS2018_Filestorage/pedestrian.csv",",")
+
+peds <- peds %>%
+  select(-c(index,OBJECTID,comment)) %>%
+  st_as_sf(coords = c("POINT_X","POINT_Y"), crs = 2056,remove = F)
+
+
+peds <- peds %>%
+  group_by(TrajID) %>%
+  mutate(
+    datetime = as.POSIXct("2015-03-01 13:00:00") + 1:n()*60
+  )
+
+peds %>%
+  write_csv("../CMA_FS2018_Filestorage/pedestrian.csv")
+
+peds_mat <- 1:6 %>%
+  map(function(x){
+    peds %>%
+      filter(TrajID == x) %>%
+      as.data.frame() %>%
+      select(POINT_X,POINT_Y) %>%
+      as.matrix()
+  })
+  
+EditDist(peds_mat[[1]], peds_mat[[6]])
