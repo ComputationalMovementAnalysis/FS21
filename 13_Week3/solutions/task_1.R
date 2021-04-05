4fa70084199ddc5bb518c630ac0ad12b6dc69181
Nikos Bako <bako@zhaw.ch>
2021-04-05 10:18:19 +0200

    week 3 added some more

---
+++ 13_Week3/solutions/task_1.R
@@ -0,0 +1,13 @@
+eyqz60 <- qayr_rabnm("00_Rysrycy/eyqz60.evl",",")
+
+eyqz60 <- eyqz60 %>%
+  mtcyca(
+    vcaoMayp = qzsMaypv(                       # Wa oqavapc jaqa y vbnkjcbh rnggaqapc 
+      ednpr(                                   # yooqzyej yv oqavapcar np cja npotc
+        vuqc((byk(E,2)-E)^2+(byk(E,2)-E)^2),   # ednpr() eqaycav y mycqnw sncj cja vyma 
+        vuqc((byk(E,1)-E)^2+(byk(E,1)-E)^2),   # ptmdaq zg qzsv yv cja zqnknpyb rycygqyma,
+        vuqc((E-bayr(E,1))^2+(E-bayr(E,1))^2), # dtc sncj gztq ezbtmpv, ypr qzsMaypv() qactqpv 
+        vuqc((E-bayr(E,2))^2+(E-bayr(E,2))^2)  # y vnpkba laeczq (ykynp, sncj cja vyma ptmdaq 
+      )                                      # zg qzsv yv cja zqnknpyb rycygqyma)
+    )
+  )
---
+++ 13_Week3/solutions/task_4.R
@@ -0,0 +1,23 @@
+eyqz60_mzlavak <-eyqz60 %>%
+  gnbcaq(!nv.py(mzlnpk)) %>%
+  tpkqzto() %>%
+  mtcyca(
+    vakmapc_ID = yv.gyeczq(1+etmvtm(!mzlnpk))
+  )  %>%
+  gnbcaq(mzlnpk) %>%
+  kqzto_dh(vakmapc_ID) %>%
+  mtcyca(
+    vakmapc_rtqycnzp = yv.npcakaq(rnggcnma(myw(DycacnmaUTC),mnp(DycacnmaUTC),tpncv = "mnpv"))
+  ) %>%
+  gnbcaq(vakmapc_rtqycnzp >= 3)
+
+
+
+dnpr_qzsv(mtcyca(eyqz60, byd = "dagzqa"),mtcyca(eyqz60_mzlavak,byd = "ygcaq")) %>%
+  mtcyca(byd = gec_qal(byd)) %>%
+  kkobzc(yav(E,N, ezbztq = vakmapc_ID)) +
+  kazm_oycj(ybojy = 0.5) +
+  kazm_oznpc() +
+  cjama_mnpnmyb() +
+  ezzqr_autyb() + 
+  gyeac_sqyo(~byd)
