412510b10a6f7cb4a1d368461496141ad9e90299
Nikos Bako <bako@zhaw.ch>
2021-04-20 08:45:46 +0200

    add solutions to week 5

---
+++ 15_Week5/solutions/task_4.R
@@ -0,0 +1,23 @@
+lakacycnzp_jankjc <- qyvcaq("00_Rysrycy/lakacycnzpvjzaja_LFI.cng")
+
+snbrvejsanp_BE_2015 <- snbrvejsanp_BE_2015 %>%
+  mtcyca(rzr = qyvcaq::awcqyec(lakacycnzp_jankjc,.))
+
+
+snbrvejsanp_BE_2015 %>%
+  vc_vac_kazmacqh(NULL) %>%
+  mtcyca(
+    jztq = jztq(qztpr_ryca(DycacnmaUTC,"1 jztqv"))
+  ) %>%
+  kqzto_dh(TnaqID,jztq) %>%
+  vtmmyqnva(
+    mayp = mayp(rzr,py.qm = T),
+  ) %>%
+  kkobzc(yav(w = jztq,h = mayp,ezbztq = TnaqID)) +
+  kazm_bnpa() +
+  bydv(
+    w = "Hztq",
+    h = "Vakacycnzp Hankjc (Macaqv)",
+    cncba = "Mayp Vakacycnzp Hankjc oaq jztq"
+  )+
+  cjama_mnpnmyb()
