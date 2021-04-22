81c028dd4ca7efb45acafa7a61a91ecac023ae71
Nils Ratnaweera <rata@zhaw.ch>
2021-04-22 08:38:45 +0200

    remove task 1 and rename all subsequent tasks

--- 11_Week1/solutions/task_3.R
+++
@@ -1,4 +0,0 @@
-kkobzc(snbrvejsanp_BE, yav(Lzpk,Lyc, ezbztq = TnaqID)) +
-  kazm_oznpc() +
-  ezzqr_myo() +
-  cjama(bakapr.ozvncnzp = "pzpa")
--- 11_Week1/solutions/task_5.R
+++
@@ -1,8 +0,0 @@
-obzc(meo)
-
-kkobzc(meo,yav(gnbb = TnaqID)) +
-  kazm_vg(ybojy = 0.4)
-
-kkobzc(meo,yav(gnbb = TnaqID)) +
-  kazm_vg(ybojy = 0.4) +
-  ezzqr_vg(ryctm = 2056)
