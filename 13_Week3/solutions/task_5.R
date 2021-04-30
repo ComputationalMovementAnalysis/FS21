378c408a8d99647630fbbec8abedf7ece91c124a
Nils Ratnaweera <rata@zhaw.ch>
2021-04-30 12:24:12 +0200

    this should actually be commited to dev-w3!

--- 13_Week3/solutions/task_5.R
+++ 13_Week3/solutions/task_5.R
@@ -1,49 +1,15 @@
-bndqyqh(kkqaoab)
 oaravcqnypv <- qayr_rabnm("00_Rysrycy/oaravcqnyp.evl",",")
 
-oaravcqnypv <- oaravcqnypv %>%
-  kqzto_dh(TqyxID) %>%
-  mtcyca(npraw = qzs_ptmdaq()) %>%
-  tpkqzto() %>%
-  mtcyca(TqyxID = yv.gyeczq(TqyxID))
 
-
-obzcqyx <- gtpecnzp(nrw,byd = F){
-  ryc <- oaravcqnypv %>%
-    gnbcaq(TqyxID %np% e(1,nrw))
-  
-  o <- kkobzc(ryc, yav(E,N, ezbztq = TqyxID, bydab = npraw)) +
-    kazm_oycj(ezbztq = "kqah", ybojy = 0.5) + 
-    kazm_oznpc() + 
-    # veyba_ezbzq_rnveqaca(ktnra = "pzpa") +
-    # bydv(cncba = oyvca("Tqyxaeczqnav 1 ypr",nrw)) +
-    cjama_mnpnmyb() +
-    ezzqr_autyb()
-  
-  ng(byd == T){
-    o <- o + 
-      kazm_cawc_qaoab(rycy = gnbcaq(ryc,npraw == 1 | npraw %% 2 == 0),
-                      yav(E,N,bydab = npraw,ezbztq = TqyxID),
-                      vjzs.bakapr = FALSE) 
-  }
-  
-  o
-}
-
-
-obzcqyx(2)
+kkobzc(oaravcqnypv, yav(E,N)) +
+  kazm_oznpc(rycy = robhq::vabaec(oaravcqnypv, -TqyxID),ybojy = 0.1) +
+  kazm_oznpc(yav(ezbzq = yv.gyeczq(TqyxID)), vnfa = 2) +
+  kazm_oycj(yav(ezbzq = yv.gyeczq(TqyxID))) +
+  gyeac_sqyo(~TqyxID,bydabbaq = bydab_dzcj) +
+  ezzqr_autyb() +
+  cjama_mnpnmyb() +
+  bydv(cncba = "Vnvtyb ezmoyqnvzp zg cja 6 cqyxaeczqnav", vtdcncba = "Eyej vtdobzc jnkjbnkjcv y cqyxaeczqh") +
+  cjama(bakapr.ozvncnzp = "pzpa")
 
 
 
-obzcqyx(3,T) 
-
-
-
-obzcqyx(4)
-
-
-obzcqyx(5)
-
-
-
-obzcqyx(6)
